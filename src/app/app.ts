b3f3d08de6df217d978c97706e2485d096985e27
si3aa <mahmoud22said22@gmail.com>
2025-09-08 07:40:58 +0300

    upload project files and use interval from rxjs to print values in console

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('rxjs');
+export class AppComponet implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const sub = interval(1000).subscribe({
+      next: (val) => console.log(val),
+    });
+    this.destroyRef.onDestroy(()=>{
+      sub.unsubscribe();
+    })
+  }
 }
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import { AppComponet } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = () => bootstrapApplication(AppComponet, config);
 
 export default bootstrap;
