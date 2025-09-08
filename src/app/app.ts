0f72de8ba10a3ca737602f6c29225896db2c6207
si3aa <mahmoud22said22@gmail.com>
2025-09-08 08:14:32 +0300

    use signal to same role of observable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { interval } from 'rxjs';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +15,29 @@ import { interval } from 'rxjs';
   styleUrl: './app.css',
 })
 export class AppComponet implements OnInit {
+  clickCount = signal(0);
+  interval = signal(0);
+  doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
-  ngOnInit(): void {
-    const sub = interval(1000).subscribe({
-      next: (val) => console.log(val),
+  constructor() {
+    effect(() => {
+      console.log(`Clicked Button ${this.clickCount()} times.`);
     });
-    this.destroyRef.onDestroy(()=>{
-      sub.unsubscribe();
-    })
+  }
+  ngOnInit(): void {
+    setInterval(() => {
+      this.interval.update((prevInterval) => prevInterval + 1);
+    });
+    // const sub = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   sub.unsubscribe();
+    // });
+  }
+  onClick() {
+    this.clickCount.update((prevClick) => prevClick + 1);
   }
 }
